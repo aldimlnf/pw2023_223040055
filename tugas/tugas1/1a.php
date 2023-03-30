28e94b0df79af59ad9f91cee07ff2f21f42f73ad
Jitaaa <116050905+Lychtael@users.noreply.github.com>
2023-03-30 15:47:59 +0700

    Create 1a.php

---
+++ tugas/tugas1/1a.php
@@ -0,0 +1,20 @@
+<?php
+    $barang = 'Topi';
+    $bentuk = 'Bundar';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>
+       <i>
+       "<?php echo $barang;?> saya <?php echo $bentuk;?>, <?php echo $bentuk;?><?php echo $barang;?> saya "
+       </i> 
+    </h2>
+</body>
+</html>
